fae0bacca5ca5359d45975f92631ca0ea8a4cb5f
neilpascual <neilaronpascual.dev@gmail.com>
2025-10-08 11:29:33 +0800

    added new project: kapet't bahay

--- src/data/projects.js
+++ src/data/projects.js
@@ -10,7 +10,7 @@ export const projects = [
   {
     title: 'Portfolio Website',
     desc: 'My personal portfolio website built with React.js.',
-    tags: ['Next.js', 'Strapi', 'Cloudinary'],
+    tags: ['React', 'Tailwind', 'Framer-Motion'],
     live: '#',
     img: '/projects/neilp.png',
     code: 'https://github.com/neilpascual/Portfolio'
@@ -30,5 +30,13 @@ export const projects = [
     live: 'https://tiktakneil.vercel.app',
     img: '/projects/tiktakneil.png',
     code: 'https://github.com/neilpascual/tictactoe'
+  },
+    {
+    title: "Kape't Bahay",
+    desc: 'A coffee shop website with a modern design, built using React, Tailwind and Framer-Motion.',
+    tags: ['React', 'Tailwind', 'Framer-Motion'],
+    live: 'https://kapet-bahay.vercel.app',
+    img: '/projects/kapetbahay.png',
+    code: 'https://github.com/neilpascual/kapet-bahay'
   },
 ];
