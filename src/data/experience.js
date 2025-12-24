ceee13c10408eeec4531bace14243e8af8d6feae
neilpascual <neilaronpascual.dev@gmail.com>
2025-12-24 10:53:29 +0800

    feat: added experiences

--- src/data/experience.js
+++ src/data/experience.js
@@ -10,7 +10,10 @@ export const experience = [
     org: 'FullSuite',
     date: 'Sep 2025 — Dec 2025',
     bullets: ['Implemented reusable components', 'Automated testing pipelines',
-      "Implement Apps Script in the Company's WorkSpace"
+      "Implement Apps Script in the Company's WorkSpace","Collaborated with cross-functional teams to define project requirements and deliverables","Assisted in debugging and resolving software issues to enhance application performance",
+      "Participated in code reviews to maintain code quality and best practices","Contributed to documentation for software features and updates","Worked closely with senior developers to gain insights into software development processes",
+      "Engaged in team meetings and brainstorming sessions to foster innovative solutions","Maintained version control using Git and GitHub for collaborative development","Learned and adapted to new technologies and frameworks as required by projects",
+      "Designed user interfaces using React.js and Tailwind CSS to enhance user experience","Designed user interfaces using figma to enhance user experience",
     ],
   },
 ];
